dd8e36810021d2ad4c89e23b7d58f97597db43fc
Nick Perrin <nick.perrin@hotmail.com>
2024-10-30 21:51:49 -0400

    Toolbar options to access debugger

--- Editor/Scripts/InputToolbarOptions.cs
+++ Editor/Scripts/InputToolbarOptions.cs
@@ -1,3 +1,4 @@
+using NPTP.InputSystemWrapper.Editor.EditorWindows;
 using UnityEditor;
 using Object = UnityEngine.Object;
 
@@ -9,23 +10,30 @@ namespace NPTP.InputSystemWrapper.Editor
         private const string REGENERATE_INPUT_CODE = TOOLBAR_NAME + "/Regenerate Input Wrapper Code";
         private const string OFFLINE_INPUT_DATA = TOOLBAR_NAME + "/Offline Input Data";
         private const string RUNTIME_INPUT_DATA = TOOLBAR_NAME + "/Runtime Input Data";
+        private const string OPEN_DEBUGGER_WINDOW = TOOLBAR_NAME + "/Open Input Wrapper Debugger Window";
 
-        [MenuItem(REGENERATE_INPUT_CODE)]
+        [MenuItem(REGENERATE_INPUT_CODE, isValidateFunction: false, 0)]
         private static void RegenerateInputCode()
         {
             InputScriptGenerator.GenerateInputScriptCode();
         }
 
-        [MenuItem(OFFLINE_INPUT_DATA)]
+        [MenuItem(OFFLINE_INPUT_DATA, isValidateFunction: false, 100)]
         private static void OfflineInputData()
         {
             SelectAsset(Helper.OfflineInputData);
         }
 
-        [MenuItem(RUNTIME_INPUT_DATA)]
+        [MenuItem(RUNTIME_INPUT_DATA, isValidateFunction: false, 100)]
         private static void RuntimeInputData()
         {
             SelectAsset(Helper.OfflineInputData.RuntimeInputData);
+        }
+
+        [MenuItem(OPEN_DEBUGGER_WINDOW, isValidateFunction: false, 200)]
+        private static void OpenDebuggerWindow()
+        {
+            EditorWindow.GetWindow(typeof(InputWrapperDebuggerWindow));
         }
 
         private static void SelectAsset(Object asset)
