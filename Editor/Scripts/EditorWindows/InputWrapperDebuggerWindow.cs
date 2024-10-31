c5c766468afd0237f68199b6595cb524831c6f6f
Nick Perrin <nick.perrin@hotmail.com>
2024-10-30 21:45:39 -0400

    Implement first part of InputWrapperDebuggerWindow

---
+++ Editor/Scripts/EditorWindows/InputWrapperDebuggerWindow.cs
@@ -0,0 +1,50 @@
+using System.Drawing;
+using UnityEditor;
+using UnityEngine;
+using FontStyle = UnityEngine.FontStyle;
+
+namespace NPTP.InputSystemWrapper.Editor.EditorWindows
+{
+	public class InputWrapperDebuggerWindow : EditorWindow
+	{
+		private const string EMPTY = "";
+		
+		// Updates the window when in play mode, so it shows up-to-date runtime debug info.
+		public void OnInspectorUpdate()
+		{
+			if (!Application.isPlaying)
+			{
+				return;
+			}
+			
+			Repaint();
+		}
+		
+		private void OnGUI()
+		{
+			if (!Application.isPlaying)
+			{
+				EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+				EditorGUILayout.LabelField("You must be in play mode to use the debugger.", new GUIStyle(EditorStyles.label) { fontStyle = FontStyle.BoldAndItalic });
+				return;
+			}
+			
+			GUILayout.BeginVertical();
+			ShowDebugInfoField("Current Control Scheme", Input.CurrentControlScheme.ToString());
+			ShowDebugInfoField("Current Context", Input.Context.ToString());
+			GUILayout.EndVertical();
+		}
+
+		private static void ShowDebugInfoField(string boldLabel, string info = EMPTY)
+		{
+			string labelExtended = boldLabel + ": ";
+			GUIStyle boldStyle = new GUIStyle(EditorStyles.label) { fontStyle = FontStyle.Bold };
+			Vector2 labelSize = boldStyle.CalcSize(new GUIContent(labelExtended));
+			
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField(labelExtended, boldStyle, GUILayout.Width(labelSize.x));
+			EditorGUILayout.LabelField(info, EditorStyles.label);
+			EditorGUILayout.EndHorizontal();
+		}
+	}
+}
