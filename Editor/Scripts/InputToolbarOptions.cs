5fa34ab30e9ca081e2cde22fe00cd2723ac98aa0
Nick Perrin <nickptperrin@outlook.com>
2026-08-30 00:50:20 -0400

    Fixes

--- Editor/Scripts/InputToolbarOptions.cs
+++ Editor/Scripts/InputToolbarOptions.cs
@@ -7,12 +7,12 @@ namespace NPTP.InputSystemWrapper.Editor
     internal static class InputToolbarOptions
     {
         private const string TOOLBAR_NAME = "Input";
-        private const string REGENERATE_INPUT_CODE = TOOLBAR_NAME + "/Regenerate Input Wrapper Code";
+        private const string REGENERATE_INPUT_CODE_ASSETS = TOOLBAR_NAME + "/Regenerate Input Wrapper Code & Assets";
         private const string OFFLINE_INPUT_DATA = TOOLBAR_NAME + "/Offline Input Data";
         private const string RUNTIME_INPUT_DATA = TOOLBAR_NAME + "/Runtime Input Data";
         private const string OPEN_DEBUGGER_WINDOW = TOOLBAR_NAME + "/Input Wrapper Debugger Window";
 
-        [MenuItem(REGENERATE_INPUT_CODE, isValidateFunction: false, 0)]
+        [MenuItem(REGENERATE_INPUT_CODE_ASSETS, isValidateFunction: false, 0)]
         private static void RegenerateInputCode()
         {
             InputScriptGenerator.GenerateInputScriptCode();
