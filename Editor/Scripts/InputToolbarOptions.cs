ead676987942f3e88c6168a8f5ba7d0680fe09a7
Nick Perrin <nickptperrin@outlook.com>
2026-08-30 01:02:16 -0400

    Replace ampersand

--- Editor/Scripts/InputToolbarOptions.cs
+++ Editor/Scripts/InputToolbarOptions.cs
@@ -7,7 +7,7 @@ namespace NPTP.InputSystemWrapper.Editor
     internal static class InputToolbarOptions
     {
         private const string TOOLBAR_NAME = "Input";
-        private const string REGENERATE_INPUT_CODE_ASSETS = TOOLBAR_NAME + "/Regenerate Input Wrapper Code & Assets";
+        private const string REGENERATE_INPUT_CODE_ASSETS = TOOLBAR_NAME + "/Regenerate Input Wrapper Code and Assets";
         private const string OFFLINE_INPUT_DATA = TOOLBAR_NAME + "/Offline Input Data";
         private const string RUNTIME_INPUT_DATA = TOOLBAR_NAME + "/Runtime Input Data";
         private const string OPEN_DEBUGGER_WINDOW = TOOLBAR_NAME + "/Input Wrapper Debugger Window";
